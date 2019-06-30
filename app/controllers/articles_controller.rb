2f235bfd53a8ba294da2c8af03cf1d27ace63324
Thorny Bastard <thorny@startmail.com>
2019-06-30 16:42:41 -0400

    Partially Destroyed?
    Added partials and destroy function

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
 
 def index
-  @list_of_articles = Article.all 
+  @list_of_articles = Article.all
 end
 
   def new
@@ -34,6 +34,13 @@ end
 
   def show
     @article = Article.find(params[:id])
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article destroyed"
+    redirect_to article_path
   end
 
   private
