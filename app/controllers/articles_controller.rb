b3b5d82279b540ac694881cf65f18837921bad57
Thorny Bastard <thorny@startmail.com>
2019-06-30 15:42:04 -0400

    show, edit, links
    added links to show, edit, and create pages, created article index, and some other stuff.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,15 @@
 class ArticlesController < ApplicationController
+
+def index
+  @list_of_articles = Article.all 
+end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -13,8 +22,18 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] = "Your article has been updated"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   def show
-    @article = Article.find(params[:id])      
+    @article = Article.find(params[:id])
   end
 
   private
