6ae4c5e487763d2a46a7f4d0f653eeb1dc5be26f
Thorny Bastard <thorny@startmail.com>
2019-07-19 12:37:52 -0400

    Flash messages
    styles flash messages in partials to have green and red backgrounds and little 'x' for closing color bar. All styled errors in errors partial.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ end
   def create
     @article = Article.new(article_params)
     if @article.save
-      flash[:notice] = "Your article was successfully posted."
+      flash[:success] = "Your article was successfully posted."
       redirect_to article_path(@article)
     else
       render 'new'
@@ -24,7 +24,7 @@ end
 
   def update
     if @article.update(article_params)
-      flash[:notice] = "Your article has been updated"
+      flash[:success] = "Your article has been updated"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -36,7 +36,7 @@ end
 
   def destroy
     @article.destroy
-    flash[:notice] = "Article destroyed"
+    flash[:danger] = "Article destroyed"
     redirect_to articles_path
   end
 
