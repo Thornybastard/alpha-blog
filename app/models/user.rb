253a1df192afc88333d88ea530b294a7997651e3
Thorny Bastard <thorny@startmail.com>
2019-07-27 12:55:31 -0400

    authentication
    added password for authentication including password_digest column to User table and gem bcrypt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,9 @@ class User < ApplicationRecord
   has_many :articles
   before_save { self.email = email.downcase }
   validates :username, presence: true, uniqueness: { case_sensitive: false },
-  length: { minimum: 3, maximum: 50 }
+            length: { minimum: 3, maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, uniqueness: { case_sensitive: false },
-  format: { with: VALID_EMAIL_REGEX }
+            format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
